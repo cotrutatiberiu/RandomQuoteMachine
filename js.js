9711f569994f76561db35c90060113a0ecbfb3a9
cotrutatiberiu <32967624+cotrutatiberiu@users.noreply.github.com>
2017-11-18 12:47:33 +0200

    final

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
-var quote1 = new Quote("a", "b");
-var quote2 = new Quote("w", "z");
-var quote3 = new Quote("e", "x");
-var quote4 = new Quote("r", "c");
+var quote1 = new Quote("Don't cry because it's over, smile because it happened.", "Dr. Seuss");
+var quote2 = new Quote("I'm selfish, impatient and a little insecure. I make mistakes, <br></br>I am out of control and at times hard to handle.<br></br> But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.", "Marilyn Monroe");
+var quote3 = new Quote("Be yourself; everyone else is already taken.", "Oscar Wilde");
+var quote4 = new Quote("Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.", "Albert Einstein");
 var numberQuotes = 4;
 
 var arrayObjects = [];
@@ -33,9 +33,18 @@ function randomNumberPick() {
 
 function printQuote() {
   var numberQuoteChosen = randomNumberPick();
-  document.write(arrayObjects[numberQuoteChosen].quote);
-  document.write(arrayObjects[numberQuoteChosen].author);
+  // document.write(arrayObjects[numberQuoteChosen].quote);
+  // document.write(arrayObjects[numberQuoteChosen].author);
+  quoteE.innerHTML= arrayObjects[numberQuoteChosen].quote;
+  authorE.innerHTML= arrayObjects[numberQuoteChosen].author;
 }
 
+
+
+var button=document.getElementById("newQ");
+var quoteE=document.getElementById("quoteElem");
+var authorE=document.getElementById("authorElem");
+
+button.addEventListener("click",printQuote);
 createArrayOfObjects();
 printQuote();
